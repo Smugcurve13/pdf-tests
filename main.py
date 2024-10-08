e168b2d6428978ed872b187218cfc81fb8e635c2
Smugcurve13 <sambhavsoni14@gmail.com>
2024-10-08 12:20:17 +0530

    added body of text

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ filepaths = glob.glob("files/*.txt")
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 
 for filepath in filepaths:
-    # file = open(filepath,'r')
     pdf.add_page()
+
+    # Add a title
     filename =Path(filepath).stem
     pdf.set_font(family='Times',style='b',size=20)
     pdf.cell(w=50,h=8,txt=f"{filename.title()}",ln=1)
-    
+
+    # Add body of file
+    pdf.set_font(family='Times',size=10)
+    with open(filepath,'r') as file:
+        pdf.multi_cell(w=180,h=8,txt=file.read())
+
 pdf.output('PDFs/output.pdf')
